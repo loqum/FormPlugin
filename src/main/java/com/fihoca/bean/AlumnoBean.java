5e2770d4dd0286448a19a0460b61328d7e3fe383
Ruben <ruben693@gmail.com>
2018-08-29 16:53:49 +0200

    commit

--- src/main/java/com/fihoca/bean/AlumnoBean.java
+++ src/main/java/com/fihoca/bean/AlumnoBean.java
@@ -1,9 +1,20 @@
 package com.fihoca.bean;
 
+import java.io.File;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import lombok.Data;
 
@@ -29,12 +40,62 @@ public class AlumnoBean implements Serializable {
 		this.apellidos = apellidos;
 		this.dni = dni;
 	}
-	
-	public String add() {
+
+	public String add(AlumnoBean alumno) throws Exception {
 		System.out.println("Entra dentro del método add");
-		
+
+		DocumentBuilderFactory factory = null;
+		DocumentBuilder builder = null;
+		Document document = null;
+
+		try {
+			factory = DocumentBuilderFactory.newInstance();
+			builder = factory.newDocumentBuilder();
+			document = builder.newDocument();
+
+			// definimos el elemento raíz del documento
+			Element eRaiz = document.createElement("Alumnos");
+			document.appendChild(eRaiz);
+
+			// definimos el nodo que contendrá los elementos
+			Element eAlumno = document.createElement("Alumno");
+			eRaiz.appendChild(eAlumno);
+
+			// atributo para el nodo coche
+			Attr atributo = document.createAttribute("id");
+			atributo.setValue(String.valueOf(alumno.getIdAlumno()));
+			eAlumno.setAttributeNode(atributo);
+
+			// definimos cada uno de los elementos y le asignamos un valor
+			Element eNombre = document.createElement("Nombre");
+			eNombre.appendChild(document.createTextNode(alumno.getNombre()));
+			eAlumno.appendChild(eNombre);
+
+			Element eApellidos = document.createElement("Apellidos");
+			eApellidos.appendChild(document.createTextNode(alumno.getApellidos()));
+			eAlumno.appendChild(eApellidos);
+
+			Element eDNI = document.createElement("DNI");
+			eDNI.appendChild(document.createTextNode(alumno.getDni()));
+			eAlumno.appendChild(eDNI);
+
+			// clases necesarias finalizar la creación del archivo XML
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(document);
+			StreamResult result = new StreamResult(new File("C:\\alumno.xml"));
+
+			transformer.transform(source, result);
+
+			System.out.println("File saved");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+
+		}
 		return "view";
 	}
-	
 
 }
--- src/main/java/com/fihoca/utilities/FieldsValidator.java
+++ src/main/java/com/fihoca/utilities/FieldsValidator.java
@@ -4,10 +4,11 @@ import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
 @FacesValidator("com.fihoca.utilities.FieldsValidator")
-public class FieldsValidator {
+public class FieldsValidator implements Validator {
 
 	public FieldsValidator() {
 
@@ -17,7 +18,7 @@ public class FieldsValidator {
 
 		if (value.equals(null)) {
 
-			FacesMessage msg = new FacesMessage("Value is required");
+			FacesMessage msg = new FacesMessage();
 			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
 			throw new ValidatorException(msg);
 
